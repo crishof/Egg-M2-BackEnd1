fcbf4ba5255b477e78ad382e096752c5861663e5
Cristian Hoffmann <cristianhof@gmail.com>
2023-05-30 09:49:20 +0200

    Add Extras guia10

---
+++ Guia10Ex01/src/Main.java
@@ -0,0 +1,9 @@
+/*
+1. Diseñar un programa que lea una serie de valores numéricos enteros desde el teclado
+y los guarde en un ArrayList de tipo Integer. La lectura de números termina cuando se
+introduzca el valor -99. Este valor no se guarda en el ArrayList. A continuación, el
+programa mostrará por pantalla el número de valores que se han leído, su suma y su
+media (promedio).
+ */
+public class Main {
+}
